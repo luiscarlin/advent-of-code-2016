1ceb9e9aec8f86e5bf0995dd9df7d74cb8a39e17
Luis Carlin <luiscarlin@gmail.com>
2019-01-08 06:16:49 -0500

    day4 part2 done

--- 4.py
+++ 4.py
@@ -1,5 +1,23 @@
 from collections import defaultdict
 
+def shift_by_one(string_to_shift):
+  new_string = ''
+
+  for ch in string_to_shift:
+    if ch != ' ' :
+      if ch is 'z':
+        replacement = 'a'
+      else:
+        replacement = chr(ord(ch) + 1)
+    else:
+      replacement = " "
+
+    new_string += replacement
+
+  return new_string
+
+rooms = []
+
 total = 0
 
 for line in open('./4.in').read().split('\n'):
@@ -28,5 +46,15 @@ for line in open('./4.in').read().split('\n'):
 
     if most_common == checksum:
       total += int(id)
+      rooms.append((' '.join(words[:-1]), int(id)))
 
 print('part 1', total)
+
+for room in rooms:
+  name, id = room[0], room[1]
+
+  for _ in range(id):
+    name = shift_by_one(name)
+
+  if 'orth' in name:
+    print(name, id)
