b2814a5481420a3f44ff596c61398260fff682e2
Luis Carlin <luiscarlin@gmail.com>
2019-01-10 06:20:32 -0500

    day5 part 1

--- 5.py
+++ 5.py
@@ -10,7 +10,19 @@ while len(password) != 8:
     password += next_char_in_pass
 '''
 
+import hashlib
 
 door_id = 'cxdnnyjw'
 
-print(door_id)
+password = ''
+
+i = 0
+while len(password) != 8:
+  hash = hashlib.md5('{}{}'.format(door_id, i).encode('utf-8')).hexdigest()
+
+  if hash.startswith('00000'):
+    password += hash[5]
+
+  i += 1
+
+print('part 1', password)
