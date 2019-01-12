b7377f341a6312f815df78db734cd01ff41a4e4a
Luis Carlin <luiscarlin@gmail.com>
2019-01-12 10:10:43 -0500

    day 5 part 2 done

--- 5.py
+++ 5.py
@@ -4,19 +4,28 @@ import hashlib
 def get_password(sequential):
   door_id = 'cxdnnyjw'
 
-  password = ''
+  password = [-1] * 8
 
   i = 0
-  while len(password) != 8:
+  while -1 in password:
     hash = hashlib.md5('{}{}'.format(door_id, i).encode('utf-8')).hexdigest()
 
     if hash.startswith('00000'):
       if sequential:
-        password += hash[5]
+        index = password.index(-1)
+        password[index] = hash[5]
       else:
-        # do it based on position
+        position = hash[5]
+        char = hash[6]
+
+        if str.isdigit(position):
+          position = int(position)
+
+          if position in range(0, 8) and password[position] is -1:
+            password[position] = char
 
     i += 1
-  return password
+  return ''.join(password)
 
 print('part 1', get_password(sequential=True))
+print('part 2', get_password(sequential=False))
