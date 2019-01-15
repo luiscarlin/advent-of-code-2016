de7909ef7cf3cd6db64def7cbde19fa11d4e0154
Luis Carlin <luiscarlin@gmail.com>
2019-01-15 05:52:28 -0500

    start day 8

--- 8.py
+++ 8.py
@@ -1,2 +1,13 @@
+'''
+- 50 pixels wide and 6 pixels tall
+- start off
+
+
+- rect AxB turns on all of the pixels in a rectangle at the top-left of the screen which is A wide and B tall.
+- rotate row y=A by B shifts all of the pixels in row A (0 is the top row) right by B pixels. Pixels that would fall off the right end appear at the left end of the row.
+- rotate column x=A by B shifts all of the pixels in column A (0 is the left column) down by B pixels. Pixels that would fall off the bottom appear at the top of the column.
+'''
+
 for line in open('8.in'):
-  print(line)
+  if line:
+    if 'rect' in line:
